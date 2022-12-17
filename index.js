8d132f5a236fc01858cf8d8de10e8eca6739cb84
Imac <imac@Imacs-iMac.local>
2022-12-17 19:15:47 +0700

    update header access

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ app.use(bodyParser.json({ limit: "20mb" }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 var corsOptions = {
   //origin: "http://localhost:8081",
   origin: process.env.URL_FRONTEND,
